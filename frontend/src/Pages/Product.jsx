04d9d895ef827da7c9840d9c2c5811a8ed72ce72
Roy Chen <roy97chen@gmail.com>
2024-07-18 23:43:06 +0300

    after RelatedProducts

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom'
 import Breadcrum from '../Components/Breadcrums/Breadcrum'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
 import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -16,6 +17,7 @@ const Product = () => {
       <Breadcrum product={product}/>
       <ProductDisplay product={product}/> 
       <DescriptionBox />
+      <RelatedProducts />
     </div>
   )
 }
