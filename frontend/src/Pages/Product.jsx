b063fbe9b219a9fdf5938dec1373d2c38ccf12d2
Roy Chen <roy97chen@gmail.com>
2024-07-18 22:28:30 +0300

    after product.jsx and breadcrum

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
+import Breadcrum from '../Components/Breadcrums/Breadcrum'
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((item) => item.id === Number(productId));
+
   return (
-    <div></div>
+    <div>
+      <Breadcrum product={product}/>
+    </div>
   )
 }
 
