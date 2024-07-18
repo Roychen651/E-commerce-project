861bd3871e76bdd5550aaf8bfaa8d29b51e3a3ee
Roy Chen <roy97chen@gmail.com>
2024-07-18 21:56:43 +0300

    after login page and shop-category

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div></div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>הרשמה</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='השם שלך' />
+          <input type="email" placeholder='כתובת מייל' />
+          <input type="password" placeholder='סיסמא' />
+          
+        </div>
+        <button>המשך</button>
+        <p className="loginsignup-login">כבר יש לך חשבון? <span>התחבר כאן</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>בהרשמתי , אני מאשר את תנאי השימוש והפרטיות</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item';
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+
+  console.log('all_product:', all_product); // Add this line to verify the data
+
+  if (!all_product) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div></div>
-  )
-}
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>מציג 1-12</span> מתוך 36 מוצרים
+        </p>
+        <div className="shopcategory-sort">
+          מיין לפי <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <div className="shopcategory-loadmore">
+        טען עוד
+        
+      </div>
+    </div>
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
