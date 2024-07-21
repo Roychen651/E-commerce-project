5c5219a064679d760bfb1dc422f56b391a1ff367
Roy Chen <roy97chen@gmail.com>
2024-07-21 11:24:18 +0300

    After initilize Admin Panel files

---
+++ admin/src/assets/data.js
@@ -0,0 +1,40 @@
+import p1_img from './product_1.png';
+import p35_img from './product_35.png';
+import p3_img from './product_3.png';
+import p24_img from './product_24.png';
+
+let data_product = [
+  {
+    id: 1,
+    name: "חולצה עם שרוולי פלאטר וצווארון חופף",
+    image: p1_img,
+    new_price: 50.00,
+    old_price: 80.50,
+  },
+  {
+    id: 35,
+    name: "סווטשירט ילדים בצבע כתום עם כובע",
+    category: "kid",
+    image: p35_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 3,
+    name: "חולצה עם שרוולי פלאטר וצווארון חופף",
+    image: p3_img,
+    new_price: 60.00,
+    old_price: 100.50,
+  },
+  
+  {
+    id: 24,
+    name: "מעיל גברים שחור מעור עם רוכסן",
+    category: "men",
+    image: p24_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+];
+
+export default data_product;
---
+++ admin/src/assets/new_collections.js
@@ -0,0 +1,69 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_35.png";
+import p3_img from "./product_14.png";
+import p4_img from "./product_8.png";
+import p5_img from "./product_15.png";
+import p6_img from "./product_2.png";
+import p7_img from "./product_17.png";
+import p8_img from "./product_28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "חולצה עם שרוולי פלאטר וצווארון חופף",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 35,
+    name: "סווטשירט כתום עם קפוצ'ון לבנים",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "מעיל בומבר ירוק לגברים עם רוכסן מלא והתאמה צמודה",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 8,
+    name: "חולצה עם שרוולי פלאטר וצווארון חופף",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 15,
+    name: "מעיל בומבר ירוק לגברים עם רוכסן מלא והתאמה צמודה",
+    image: p5_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "חולצה עם שרוולי פלאטר וצווארון חופף",
+    image: p6_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 17,
+    name: "מעיל בומבר ירוק לגברים עם רוכסן מלא והתאמה צמודה",
+    image: p7_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 28,
+    name: "סווטשירט כתום עם קפוצ'ון לבנים",
+    image: p8_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+];
+
+export default new_collections;
